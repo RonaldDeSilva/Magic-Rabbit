4996a0f11949eeb3f4c457a637efa4a0dc7c7b6e
SnowmanWarrrior <trevmelendy@gmail.com>
2025-12-03 21:08:39 -0500

    Added Stats Screen

--- Assets/Scripts/Stats Screen.cs
+++ Assets/Scripts/Stats Screen.cs
@@ -1,16 +1,56 @@
 using UnityEngine;
+using System.Collections;
+
 
 public class StatsScreen : MonoBehaviour
+
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public int CardShuffleTime;
+    public int MaxHealth;
+    public int BaseDamage;
+    public int Luck;
+    public int StartingCardAmount;
+    public MemoryCard Meme;
+    public GameObject Buttons;
+
+   
+
     void Start()
     {
-        
+        Meme = GameObject.FindGameObjectWithTag("MemoryCard").GetComponent<MemoryCard>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        if (Meme.rabbitFeet >= 2)
+        {
+            Buttons.SetActive(true);
+        }
+        else
+        {
+            Buttons.SetActive(false);
+        }
+    }
+    public void CardShuffleTimeButton()
+    {
+        CardShuffleTime += 1;
+
+    }
+    public void MaxHealthButton()
+    {
+        MaxHealth += 1;
+    }
+    public void BaseDamageButton()
+    {
+        BaseDamage += 1;
+    }
+    public void LuckButton()
+    {
+        Luck += 1;
+    }
+    public void StartingCardAmountButton()
+    {
+        StartingCardAmount += 1;
     }
 }
