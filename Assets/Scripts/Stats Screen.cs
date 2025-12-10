23e1a74fdeb19d5806166c5d8f1c80e5aa30f16b
SnowmanWarrrior <trevmelendy@gmail.com>
2025-12-09 20:24:43 -0500

    Stats

--- Assets/Scripts/Stats Screen.cs
+++ Assets/Scripts/Stats Screen.cs
@@ -12,8 +12,14 @@ public class StatsScreen : MonoBehaviour
     public int StartingCardAmount;
     public MemoryCard Meme;
     public GameObject Buttons;
+    public GameObject[] CardShuffleTimeArray;
+    public GameObject[] MaxHealthArray;
+    public GameObject[] BaseDamageArray;
+    public GameObject[] LuckArray;
+    public GameObject[] StartingCardAmountArray;
+    
 
-   
+
 
     void Start()
     {
@@ -31,26 +37,59 @@ public class StatsScreen : MonoBehaviour
         {
             Buttons.SetActive(false);
         }
+        
+        
     }
     public void CardShuffleTimeButton()
     {
         CardShuffleTime += 1;
 
+        //If CardShuffleTime is greater than 10, set it to 10 and disable the buttons
+        if (CardShuffleTime >= 10)
+        {
+            CardShuffleTime = 10;
+            Buttons.SetActive(false);
+        }
+
     }
     public void MaxHealthButton()
     {
         MaxHealth += 1;
+
+        if (MaxHealth >= 10)
+        {
+            MaxHealth = 10;
+            Buttons.SetActive(false);
+        }
     }
     public void BaseDamageButton()
     {
         BaseDamage += 1;
+
+        if (BaseDamage >= 10)
+        {
+            BaseDamage = 10;
+            Buttons.SetActive(false);
+        }
     }
     public void LuckButton()
     {
         Luck += 1;
+
+        if (Luck >= 10)
+        {
+            Luck = 10;
+            Buttons.SetActive(false);
+        }
     }
     public void StartingCardAmountButton()
     {
         StartingCardAmount += 1;
+
+        if (StartingCardAmount >= 5)
+        {
+            StartingCardAmount = 5;
+            Buttons.SetActive(false);
+        }
     }
 }
